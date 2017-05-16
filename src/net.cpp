3978f03a644c5dd47ba57c01789fa68007ce31f5
Tim M <tim@projectradium.org>
2017-05-16 12:59:54 -0400

    Bump miniupnpc api support

--- src/net.cpp
+++ src/net.cpp
@@ -1082,11 +1082,15 @@ void ThreadMapPort()
 #ifndef UPNPDISCOVER_SUCCESS
     /* miniupnpc 1.5 */
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0);
-#else
+#elif MINIUPNPC_API_VERSION < 14
     /* miniupnpc 1.6 */
     int error = 0;
     // FIXME: ivp6 int required
     devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 0, &error);
+#else
+    /* miniupnpc 1.9.x */
+    int error = 0;
+    devlist = upnpDiscover(2000, multicastif, minissdpdpath, 0, 0, 2, &error);
 #endif
 
     struct UPNPUrls urls;
